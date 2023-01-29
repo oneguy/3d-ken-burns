b51c0e6b1843cfc224f20e06dbe4f927cab300db
Sergio Voitenko <oneguy@gmail.com>
2023-01-29 14:34:50 -0500

    Update autozoom.py
    updated to support 4K and slowing zoom-in feature

--- autozoom.py
+++ autozoom.py
@@ -53,46 +53,45 @@ arguments_strIn = './images/doublestrike.jpg'
 arguments_strOut = './autozoom.mp4'
 
 for strOption, strArgument in getopt.getopt(sys.argv[1:], '', [ strParameter[2:] + '=' for strParameter in sys.argv[1::2] ])[0]:
-	if strOption == '--in' and strArgument != '': arguments_strIn = strArgument # path to the input image
-	if strOption == '--out' and strArgument != '': arguments_strOut = strArgument # path to where the output should be stored
+  if strOption == '--in' and strArgument != '': arguments_strIn = strArgument # path to the input image
+  if strOption == '--out' and strArgument != '': arguments_strOut = strArgument # path to where the output should be stored
 # end
 
 ##########################################################
 
 if __name__ == '__main__':
-	npyImage = cv2.imread(filename=arguments_strIn, flags=cv2.IMREAD_COLOR)
+  npyImage = cv2.imread(filename=arguments_strIn, flags=cv2.IMREAD_COLOR)
 
-	intWidth = npyImage.shape[1]
-	intHeight = npyImage.shape[0]
+  intWidth = npyImage.shape[1]
+  intHeight = npyImage.shape[0]
 
-	fltRatio = float(intWidth) / float(intHeight)
+  fltRatio = float(intWidth) / float(intHeight)
 
-	intWidth = min(int(1024 * fltRatio), 1024)
-	intHeight = min(int(1024 / fltRatio), 1024)
+  intWidth = min(int(3480 * fltRatio), 3480)
+  intHeight = min(int(3480 / fltRatio), 3480)
 
-	npyImage = cv2.resize(src=npyImage, dsize=(intWidth, intHeight), fx=0.0, fy=0.0, interpolation=cv2.INTER_AREA)
+  npyImage = cv2.resize(src=npyImage, dsize=(intWidth, intHeight), fx=0.0, fy=0.0, interpolation=cv2.INTER_AREA)
+  process_load(npyImage, {})
 
-	process_load(npyImage, {})
+  objFrom = {
+    'fltCenterU': intWidth / 2.0,
+    'fltCenterV': intHeight / 2.0,
+    'intCropWidth': int(math.floor(0.97 * intWidth)),
+    'intCropHeight': int(math.floor(0.97 * intHeight))
+  }
 
-	objFrom = {
-		'fltCenterU': intWidth / 2.0,
-		'fltCenterV': intHeight / 2.0,
-		'intCropWidth': int(math.floor(0.97 * intWidth)),
-		'intCropHeight': int(math.floor(0.97 * intHeight))
-	}
+  objTo = process_autozoom({
+    'fltShift': 100.0,
+    'fltZoom': 1.10,
+    'objFrom': objFrom
+  })
 
-	objTo = process_autozoom({
-		'fltShift': 100.0,
-		'fltZoom': 1.25,
-		'objFrom': objFrom
-	})
+  npyResult = process_kenburns({
+    'fltSteps': numpy.linspace(0.0, 1.0, 200).tolist(),
+    'objFrom': objFrom,
+    'objTo': objTo,
+    'boolInpaint': True
+  })
 
-	npyResult = process_kenburns({
-		'fltSteps': numpy.linspace(0.0, 1.0, 75).tolist(),
-		'objFrom': objFrom,
-		'objTo': objTo,
-		'boolInpaint': True
-	})
-
-	moviepy.editor.ImageSequenceClip(sequence=[ npyFrame[:, :, ::-1] for npyFrame in npyResult + list(reversed(npyResult))[1:] ], fps=25).write_videofile(arguments_strOut)
+  moviepy.editor.ImageSequenceClip(sequence=[ npyFrame[:, :, ::-1] for npyFrame in npyResult ], fps=24).write_videofile(arguments_strOut)
 # end
